54c08593c9a2cdeb86deb60f3b931aeaa7dbab14
Junior <junior_a87@hotmail.com>
2026-05-14 02:49:34 +0200

    Updates done to code
    -removed old unused comments
    -added few comments
    -added -l, --list args for listing tagged files
    -added multiple source folder support
    -added some error handling

--- file_sorter.py
+++ file_sorter.py
@@ -1,95 +1,79 @@
 #!/usr/bin/env python3
 
 import os
-import shutil
 from datetime import datetime
-
 import yaml
-
-import subprocess
 import argparse
-import sys
 
-with open ("file_sort_config.yml", "r") as yml_file:
-  cfg_file = yaml.safe_load(yml_file)
-
-
-source_dir = "logs"
+yml_path = "file_sorter_config.yml"
+result_output = ""
 timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M")
-
-#pattern_matches = {"[test]": "/home/asante/code_cave/file_transfer/test_dir", "[old]": "old", "[new]": "new_dir"}
-pattern_matches = cfg_file['files_to_sort']
-check_folders = cfg_file["folders_to_check"]
-
-
-file_paths = {"text", "./text_dir/",
-              "audio", "./audio_dir",
-              "picture", "./picture_dir"
-              }
-
-
-alg = ["md5sum", "sha1sum", "sha256sum", "sha512sum"]
-provided_hash = None
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("-f", dest="file", type=str, help="Input file to calculate hash")
 argparser.add_argument("-c", dest="hash", type=str, help="Hash string to compare with")
 argparser.add_argument("-e", dest="encode", type=str, default=None, help="Encoding to use for hash calculation")
 argparser.add_argument("-a", dest="algorithm", type=int, default=2, help="0=MD5, 1=SHA1, 2=SHA256 (def), 3=SHA512")
+
+argparser.add_argument("-v", "--verbose", type=str, help="Print runtime log")
+argparser.add_argument("-t", "--tag", type=str, default=None, help="Use a specific tag for file sorter")
+argparser.add_argument("-l", "--list", action="store_true", help="List files with tags")
 args = argparser.parse_args()
 
-current_folder_to_check = check_folders["test_folder"]
-if os.path.exists(current_folder_to_check):
-  files = sorted(os.listdir(current_folder_to_check))
-  print (files)
+# Get configuration from yaml file
+if os.path.exists(yml_path):
+  with open (yml_path, "r") as yml_file:
+    cfg_file = yaml.safe_load(yml_file)
 
-  for filename in files:
-    for name_pattern in pattern_matches.keys():
-
-      # Turn create tag name tag->[tag]
-      file_tag = "[" + name_pattern + "]"
-      if filename.startswith(file_tag):
-      #if filename.startswith(name_pattern):
-
-        target_dir = pattern_matches[name_pattern]
-        if os.path.exists(target_dir):
-          print("ok")
-        else:
-          try:
-            #os.makedirs(os.path.abspath(target_dir), exist_ok=True)
-            os.makedirs(target_dir, exist_ok=True)
-            print("Directory '%s' created" % name_pattern)
-          except OSError as error:
-            print("Directory '%s' can not be created")
-          
-        new_filename = filename.replace(file_tag, "")
-        dest_path = os.path.join(target_dir, new_filename)
-        
-        os.rename(os.path.join(current_folder_to_check, filename), dest_path)
-        print(timestamp + " moved file " + filename + " to " + dest_path)
-
-  files = [f for f in files if os.path.isfile(f)]
-  print(*files, sep="\n")
-else:
-  print("Folder does not exist!: " + current_folder_to_check)
-
-      
-#source_path = os.path.join(source_dir, filename)
+files_to_sort = cfg_file['files_to_sort']
+folders_to_check = cfg_file["folders_to_check"]
 
 
-#print(list(cfg_file["files_to_sort"][new]))
+for source_folder in folders_to_check:
+  first_hit = True
+  current_folder = folders_to_check[source_folder]
+  if os.path.exists(current_folder):
+    files_in_source_folder = sorted(os.listdir(current_folder))
 
-#try:
-#    # Check if source exists
-#    if os.path.exists(source):
-#        shutil.move(source, destination)
-#        print("File move successful.")
-#    else:
-#        print(f"Error: The file {source} was not found.")
-#except PermissionError:
-#    print("Error: You don't have permission to move this file.")
-#except shutil.Error as e:
-#    print(f"A shutil error occurred: {e}")
-#except Exception as e:
-#    print(f"An unexpected error occurred: {e}")
-#
+    for filename in files_in_source_folder:
+      for tag in files_to_sort.keys():
+
+        # Create tag name tag->[tag]
+        file_tag = "[" + tag + "]"
+        if filename.startswith(file_tag):
+          if(first_hit):
+            result_output = result_output + "\t" + source_folder + "\n"
+            first_hit = False
+          if args.list:
+            result_output = result_output + filename + "\n"
+          else:
+            target_dir = files_to_sort[tag]
+            if os.path.exists(target_dir):
+              print("ok")
+            else:
+              try:
+                os.makedirs(target_dir, exist_ok=True)
+                print("Directory '%s' created" % tag)
+              except OSError as error:
+                print("Directory '%s' can not be created")
+              
+            new_filename = filename.replace(file_tag, "")
+            new_filename = new_filename.strip(' -_')
+            dest_path = os.path.join(target_dir, new_filename)
+            
+            source_file = os.path.join(current_folder, filename)
+            try:
+              if os.path.exists(source_file):
+                os.rename(source_file, dest_path)
+                result_output = result_output + timestamp + " moved file " + filename + " to " + dest_path + "\n"
+            except PermissionError:
+                print("Error: You don't have permission to move this file")
+            except FileNotFoundError:
+                print(f"Error: The file {dest_path} was not found")
+            except FileExistsError:
+                print(f"Error: The file {dest_path} already exists")
+
+  else:
+    print("Folder does not exist!: " + current_folder)
+
+print(current_folder + "\n" + result_output)
